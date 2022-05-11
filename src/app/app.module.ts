51a5d7862f60c8ce324902227fe25f7161b95085
GuillermoCarcelenMerino <guillermocarcelenmerino@gmail.com>
2022-05-11 10:34:03 +0200

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,19 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CarritoComponent } from './carrito/carrito.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CarritoComponent },
+      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   declarations: [
@@ -27,6 +31,7 @@ import { CarritoComponent } from './carrito/carrito.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CarritoComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
